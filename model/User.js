fe67c7a9820a056eaa92af60d936bbe4d8eead08
Tekleselassie Leul <edenlove2019@gmail.com>
2020-05-20 00:25:48 -0700

    router-served

--- model/User.js
+++ model/User.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 const UserSchema = new Schema({
     
     name: {
         type: String,
-        require: true,
+        required: true
     },
     phone: {
         type: String,
-        require: true,
+        required: true
     },
     email: {
         type: String,
-        require: true,
-        }
+        required: true
+    }
 
 
 
-})
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
