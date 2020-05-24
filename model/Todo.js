60cd925d2be424cdaf93b35a0232abc8ed21c13d
Tekleselassie Leul <edenlove2019@gmail.com>
2020-05-24 12:46:01 -0700

    addtodo

--- model/Todo.js
+++ model/Todo.js
@@ -2,23 +2,29 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const TodoUser = new Schema({
-	id: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'user',
-	},
-	todotext: {
-		type: String,
-		required: true,
-	},
-	createdDate: {
-		type: Date,
-		default: Date.now,
-    },
-    dueDate:{
-        type: Date
-    }
-});
+const TodoUser = new Schema(
+	
+	[{
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'user',
+		},
+		todotitle: {
+			type: String,
+			required: true
+		},
+		todotext: {
+			type: String,
+			required: true,
+		},
+		createdDate: {
+			type: Date,
+			default: Date.now,
+		},
+		dueDate: {
+			type: Date
+		}
+}]);
 
 const UserTodo = mongoose.model('UserTodo', TodoUser);
 
