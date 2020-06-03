f5f080c72c8b6fff29874e966d9f93d6fa8cba90
Tekleselassie Leul <edenlove2019@gmail.com>
2020-06-02 22:01:34 -0700

    setauthtoken

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -5,3 +5,6 @@ export const REGISTERFAIL = 'REGISTERFAIL';
 export const TODO_SUCCESS = 'TODO_SUCCESS';
 export const LOADUSER = 'LOADUSER';
 export const LOGOUT = 'LOGOUT';
+export const LOADUSER_FAIL = 'LOADUSER_FAIL';
+export const GETTODO = 'GETTODO';
+export const CLEAR_TODO = 'CLEAR_TODO';
